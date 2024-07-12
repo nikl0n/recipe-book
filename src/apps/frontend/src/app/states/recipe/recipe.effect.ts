6df2b3867d29d5972197a210c27c62e652e06028
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-07-12 13:59:47 +0200

    refactor(frontend): change constructor to inject; change exhaustMap to switchMap

--- src/apps/frontend/src/app/states/recipe/recipe.effect.ts
+++ src/apps/frontend/src/app/states/recipe/recipe.effect.ts
@@ -1,23 +1,21 @@
 import { HttpErrorResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { of } from "rxjs";
-import { catchError, exhaustMap, map } from "rxjs/operators";
+import { catchError, map, switchMap } from "rxjs/operators";
 
 import { RecipeApi } from "../../api/recipe.api";
 import { RecipeActions } from "./recipe.action";
 
 @Injectable()
 export class RecipeEffect {
-  constructor(
-    private actions$: Actions,
-    private recipeApi: RecipeApi
-  ) {}
+  actions$ = inject(Actions);
+  recipeApi = inject(RecipeApi);
 
   fetchAll$ = createEffect(() =>
     this.actions$.pipe(
       ofType(RecipeActions.fetchAll),
-      exhaustMap(() =>
+      switchMap(() =>
         this.recipeApi.fetchAllRecipes().pipe(
           map((recipes) => RecipeActions.fetchAllSuccess({ recipes })),
           catchError((error: HttpErrorResponse) => of(RecipeActions.fetchAllFailure({ error })))
