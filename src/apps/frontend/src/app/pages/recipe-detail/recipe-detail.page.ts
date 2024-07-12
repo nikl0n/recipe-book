fd11443f2c11d8b239c4044b82d15138371c898a
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-07-12 13:42:16 +0200

    feat(frontend): init recipe-detail page

---
+++ src/apps/frontend/src/app/pages/recipe-detail/recipe-detail.page.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-recipe-detail",
+  standalone: true,
+  imports: [],
+  templateUrl: "./recipe-detail.page.html",
+  styleUrl: "./recipe-detail.page.scss",
+})
+export class RecipeDetailPage {}
