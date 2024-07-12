9fc166942a04ed8fb9236e0b5dc63a34a8f1a137
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-07-12 14:01:53 +0200

    refactor(frontend): add some more recipe dummy data

--- src/apps/frontend/src/app/api/recipe.api.ts
+++ src/apps/frontend/src/app/api/recipe.api.ts
@@ -23,6 +23,18 @@ export class RecipeApi {
         image:
           "https://img.chefkoch-cdn.de/rezepte/1298241234947062/bilder/1509530/crop-960x540/carbonara-wie-bei-der-mamma-in-rom.jpg",
       },
+      {
+        id: 2,
+        name: "Lasagne",
+        image:
+          "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/69EE02DA-213D-44A2-8B08-A590225B221B/Derivates/ddaaebc0-028c-4c3c-b409-281d03dcfe19.jpg",
+      },
+      {
+        id: 3,
+        name: "Kartoffelgratin",
+        image:
+          "https://images.lecker.de/kartoffelgratin-bjpg,id=5dab1742,b=lecker,w=1600,rm=sk.webp",
+      },
     ];
 
     return of(recipe).pipe(delay(750));
