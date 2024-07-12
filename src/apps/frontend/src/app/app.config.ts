9d8f7cd852b72f58715a9e45a4d45609e8e33ca7
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-07-12 13:59:04 +0200

    refactor(frontend): add RecipeEffect and recipeFeature to app.config

--- src/apps/frontend/src/app/app.config.ts
+++ src/apps/frontend/src/app/app.config.ts
@@ -2,18 +2,23 @@ import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from "@angul
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideRouter } from "@angular/router";
 import { provideEffects } from "@ngrx/effects";
-import { provideStore } from "@ngrx/store";
+import { provideState, provideStore } from "@ngrx/store";
 import { provideStoreDevtools } from "@ngrx/store-devtools";
 
 import { routes } from "./app.routes";
+import { RecipeEffect } from "./states/recipe/recipe.effect";
+import { recipeFeature } from "./states/recipe/recipe.reducer";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+
     provideAnimationsAsync(),
+
     provideStore(),
-    provideEffects(),
+    provideEffects(RecipeEffect),
+    provideState(recipeFeature),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
