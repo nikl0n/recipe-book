d9935b237f62348bda36e099d12d0b269ce90a1c
nikgerx <github.4661w@passmail.com>
2024-07-14 16:44:14 +0200

    feat(frontend): add recipe click to recipe detail

--- src/apps/frontend/src/app/components/recipe/recipe.component.ts
+++ src/apps/frontend/src/app/components/recipe/recipe.component.ts
@@ -1,8 +1,9 @@
-import { Component, input } from "@angular/core";
+import { Component, inject, input } from "@angular/core";
 
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
+import { Router } from "@angular/router";
 import { Course } from "../../api/course.api";
 import { Recipe } from "../../api/recipe.api";
 
@@ -14,6 +15,12 @@ import { Recipe } from "../../api/recipe.api";
   styleUrl: "./recipe.component.scss",
 })
 export class RecipeComponent {
+  router = inject(Router);
+
   recipe = input.required<Recipe>();
   course = input.required<Course>();
+
+  onClickRecipe() {
+    this.router.navigateByUrl(`recipes/${this.recipe().id}`);
+  }
 }
