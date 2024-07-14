a9c43a94e24da3894565331cef8abad9a89a6193
nikgerx <github.4661w@passmail.com>
2024-07-14 16:43:16 +0200

    feat(frontend): add app.routes; recipes and recipes/:id

--- src/apps/frontend/src/app/app.routes.ts
+++ src/apps/frontend/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from "@angular/router";
+import { RecipeDetailPage } from "./pages/recipe-detail/recipe-detail.page";
 import { RecipeListPage } from "./pages/recipe-list/recipe-list.page";
 
 export const routes: Routes = [
   {
-    path: "**",
+    path: "recipes",
     component: RecipeListPage,
+  },
+  {
+    path: "recipes/:id",
+    component: RecipeDetailPage,
+  },
+  {
+    path: "**",
+    redirectTo: "recipes",
+    pathMatch: "full",
   },
 ];
