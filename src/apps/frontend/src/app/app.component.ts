1df65b0ff3b9eaf62df2a1e6e83ff9c73b42e0b7
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-08-29 15:14:21 +0200

    feat(frontend): fetch global actions in app

--- src/apps/frontend/src/app/app.component.ts
+++ src/apps/frontend/src/app/app.component.ts
@@ -1,11 +1,33 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, computed, inject, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { Store } from "@ngrx/store";
+
+import { LoadingComponent } from "./components/loading/loading.component";
+import { GlobalCategoryActions } from "./states/category/category.action";
+import { CategorySelectStatus } from "./states/category/category.reducer";
+import { GlobalUnitActions } from "./states/unit/unit.action";
+import { UnitSelectStatus } from "./states/unit/unit.reducer";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoadingComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  store = inject(Store);
+
+  categoryStatus = this.store.selectSignal(CategorySelectStatus);
+  unitStatus = this.store.selectSignal(UnitSelectStatus);
+
+  isLoading = computed(() => {
+    return [this.categoryStatus(), this.unitStatus()].some((status) => status === "LOADING");
+  });
+
+  ngOnInit() {
+    this.store.dispatch(GlobalCategoryActions.fetchAll());
+    this.store.dispatch(GlobalUnitActions.fetchAll());
+  }
+}
