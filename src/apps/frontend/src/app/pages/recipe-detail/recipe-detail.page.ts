139e53edb7be2ffa5e4679861331712a54380db5
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-08-29 15:37:44 +0200

    refactor(frontend): edit recipe-detail page to new extended recipe

--- src/apps/frontend/src/app/pages/recipe-detail/recipe-detail.page.ts
+++ src/apps/frontend/src/app/pages/recipe-detail/recipe-detail.page.ts
@@ -1,9 +1,9 @@
 import { Component, computed, effect, inject } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Store } from "@ngrx/store";
+
 import { LoadingComponent } from "../../components/loading/loading.component";
-import { CourseActions } from "../../states/course/course.action";
-import { CourseSelectCourseById, CourseSelectStatus } from "../../states/course/course.reducer";
+import { CategorySelectCategoryById } from "../../states/category/category.reducer";
 import { RecipeActions } from "../../states/recipe/recipe.action";
 import { RecipeSelectRecipeById, RecipeSelectStatus } from "../../states/recipe/recipe.reducer";
 
@@ -23,32 +23,18 @@ export class RecipeDetailPage {
   recipeStatus = this.store.selectSignal(RecipeSelectStatus);
   recipe = this.store.selectSignal(RecipeSelectRecipeById(this.paramRecipeId));
 
-  courseStatus = this.store.selectSignal(CourseSelectStatus);
-  course = this.store.selectSignal(CourseSelectCourseById(this.recipe()?.courseId ?? NaN));
+  category = this.store.selectSignal(CategorySelectCategoryById(this.recipe()?.categoryId ?? NaN));
 
   isLoading = computed(() => {
-    const statuses = [this.recipeStatus(), this.courseStatus()];
-
-    return statuses.some((status) => status === "LOADING");
+    return [this.recipeStatus()].some((status) => status === "LOADING");
   });
 
   fetchRecipeEffect = effect(
     () => {
       if (this.recipeStatus() === "IDLE" && !this.recipe()) {
         this.store.dispatch(RecipeActions.fetchById({ recipeId: this.paramRecipeId }));
+
         this.fetchRecipeEffect.destroy();
-      }
-    },
-    { allowSignalWrites: true }
-  );
-
-  fetchCourseEffect = effect(
-    () => {
-      const recipe = this.recipe();
-
-      if (recipe && this.courseStatus() === "IDLE" && !this.course()) {
-        this.store.dispatch(CourseActions.fetchById({ courseId: recipe.courseId }));
-        this.fetchCourseEffect.destroy();
       }
     },
     { allowSignalWrites: true }
