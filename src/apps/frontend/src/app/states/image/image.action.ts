29407180097e348742d924c454ec3f2446155bb3
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-09-01 01:47:45 +0200

    feat(frontend): add image fetchMany and setLastFetched actions

--- src/apps/frontend/src/app/states/image/image.action.ts
+++ src/apps/frontend/src/app/states/image/image.action.ts
@@ -1,11 +1,17 @@
 import { HttpErrorResponse } from "@angular/common/http";
-import { createActionGroup, props } from "@ngrx/store";
+import { createActionGroup, emptyProps, props } from "@ngrx/store";
 
 import { Image } from "../../api/image.api";
 
 export const ImageActions = createActionGroup({
   source: "Image",
   events: {
+    setLastFetched: props<{ componentName: string }>(),
+
+    fetchMany: emptyProps(),
+    fetchManySuccess: props<{ images: Image[] }>(),
+    fetchManyFailure: props<{ error: HttpErrorResponse }>(),
+
     fetchByRecipeId: props<{ recipeId: number }>(),
     fetchByRecipeIdSuccess: props<{ images: Image[] }>(),
     fetchByRecipeIdFailure: props<{ error: HttpErrorResponse }>(),
--- src/apps/frontend/src/app/states/image/image.reducer.ts
+++ src/apps/frontend/src/app/states/image/image.reducer.ts
@@ -7,31 +7,38 @@ interface ImageState {
   images: Image[];
   status: "IDLE" | "LOADING" | "SUCCESS" | "ERROR";
   statusAction: "CREATE" | "READ" | "UPDATE" | "DELETE";
+  lastFetched: string | null;
 }
 
 const initialState: ImageState = {
   images: [],
   status: "IDLE",
   statusAction: "READ",
+  lastFetched: null,
 };
 
 export const imageFeature = createFeature({
   name: "image",
   reducer: createReducer(
     initialState,
-    on(ImageActions.fetchByRecipeId, (state) => ({
+    on(ImageActions.setLastFetched, (state, { componentName }) => ({
+      ...state,
+      lastFetched: componentName,
+    })),
+
+    on(ImageActions.fetchMany, ImageActions.fetchByRecipeId, (state) => ({
       ...state,
       status: "LOADING" as const,
       statusAction: "READ" as const,
     })),
 
-    on(ImageActions.fetchByRecipeIdSuccess, (state, { images }) => ({
+    on(ImageActions.fetchManySuccess, ImageActions.fetchByRecipeIdSuccess, (state, { images }) => ({
       ...state,
       status: "SUCCESS" as const,
       images,
     })),
 
-    on(ImageActions.fetchByRecipeIdFailure, (state) => ({
+    on(ImageActions.fetchManyFailure, ImageActions.fetchByRecipeIdFailure, (state) => ({
       ...state,
       status: "ERROR" as const,
       images: [],
