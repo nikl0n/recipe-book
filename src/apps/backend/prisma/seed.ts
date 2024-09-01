ef938c8d2744c307779d4fa0dfeb6fa82aa0e71b
nikgerx <github.4661w@passmail.com>
2024-09-01 13:25:32 +0200

    feat(backend): add prisma seed file to fill database with required data

---
+++ src/apps/backend/prisma/seed.ts
@@ -0,0 +1,66 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const unitRecords = [
+    { id: 1, name: "x" },
+    { id: 2, name: "ml" },
+    { id: 3, name: "l" },
+    { id: 4, name: "g" },
+    { id: 5, name: "kg" },
+  ];
+
+  for (const unitRecord of unitRecords) {
+    await prisma.units.upsert({
+      where: {
+        id: unitRecord.id,
+      },
+      update: {
+        ...unitRecord,
+      },
+      create: {
+        ...unitRecord,
+      },
+    });
+  }
+
+  console.log(`upsert units :: ${unitRecords.length} items`);
+
+  const categoryRecords = [
+    { id: 1, name: "Vorspeise" },
+    { id: 2, name: "Hauptspeise" },
+    { id: 3, name: "Nachspeise" },
+    { id: 4, name: "Beilage" },
+    { id: 5, name: "Snack" },
+  ];
+
+  for (const categoryRecord of categoryRecords) {
+    await prisma.categories.upsert({
+      where: {
+        id: categoryRecord.id,
+      },
+      update: {
+        ...categoryRecord,
+      },
+      create: {
+        ...categoryRecord,
+      },
+    });
+  }
+
+  console.log(`upsert categories :: ${categoryRecords.length} items`);
+}
+
+main()
+  .then(async () => {
+    console.log("DONE");
+
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+
+    await prisma.$disconnect();
+    process.exit(1);
+  });
