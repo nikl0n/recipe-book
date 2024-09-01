b7c4fdc953bcc70de5670fcfe1f7dd9875b25478
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-09-01 05:16:46 +0200

    feat(backend): add CreateStep type

--- src/apps/backend/src/modules/step/step.controller.ts
+++ src/apps/backend/src/modules/step/step.controller.ts
@@ -2,6 +2,8 @@ import { Controller } from "@nestjs/common";
 
 import { StepService } from "./step.service";
 
+export type CreateStep = { order: number; text: string };
+
 @Controller("api/v1/steps")
 export class StepController {
   constructor(private readonly stepService: StepService) {}
