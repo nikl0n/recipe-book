6a73da1c5278c51c4ab748bb654fd6a3a3a2feb1
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-09-01 05:17:37 +0200

    feat(backend): increase json limit to 16mb

--- src/apps/backend/src/main.ts
+++ src/apps/backend/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 
+import { json } from "express";
+
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  app.use(json({ limit: "16mb" }));
 
   await app.listen(3000);
 }
