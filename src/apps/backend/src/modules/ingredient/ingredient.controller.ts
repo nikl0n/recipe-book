86f5f4266ed59aee2ab007d18230d74933e8470a
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-09-01 05:16:36 +0200

    feat(backend): add CreateIngredient type

--- src/apps/backend/src/modules/ingredient/ingredient.controller.ts
+++ src/apps/backend/src/modules/ingredient/ingredient.controller.ts
@@ -2,6 +2,8 @@ import { Controller } from "@nestjs/common";
 
 import { IngredientService } from "./ingredient.service";
 
+export type CreateIngredient = { unitId: number; name: string; amount: number };
+
 @Controller("api/v1/ingredients")
 export class IngredientController {
   constructor(private readonly ingredientService: IngredientService) {}
