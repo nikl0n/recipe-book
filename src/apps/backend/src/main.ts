0e7267c1f7d0f0bd37bbf409f1a3538874053c61
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-10-03 14:36:22 +0200

    refactor(backend): remove unneeded json limitation

--- src/apps/backend/src/main.ts
+++ src/apps/backend/src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 
-import { json } from "express";
-
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-
-  app.use(json({ limit: "3mb" }));
 
   await app.listen(3000);
 }
