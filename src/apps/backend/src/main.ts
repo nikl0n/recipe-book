4c0bab3ecfec0d819ed2166691981c9f5a8d68fe
nikgerx <github.4661w@passmail.com>
2024-10-06 23:15:03 +0200

    refactor(backend): add json limit for body, up to 3mb

--- src/apps/backend/src/main.ts
+++ src/apps/backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 
+import { json } from "express";
+
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: "3mb" }));
 
   app.enableCors();
 
