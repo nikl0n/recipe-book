be76b3dc7a72a725d7e273c37d1a3a9d61317529
Niklon <38695265+nikl0n@users.noreply.github.com>
2024-10-24 10:47:05 +0200

    refactor(types): move _index.ts to src folder; edit package.json and tsconfig.json

---
+++ src/packages/types/src/_index.ts
@@ -0,0 +1,7 @@
+export * from "./category.type.js";
+export * from "./image.type.js";
+export * from "./ingredient.type.js";
+export * from "./recipe.type.js";
+export * from "./step.type.js";
+export * from "./unit.type.js";
+export * from "./user.type.js";
